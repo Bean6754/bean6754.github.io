0d83ef04281139495c632625a63f60e0e989bb18
Matthew <Bean6754@users.noreply.github.com>
2018-10-04 15:08:35 +0100

    Update addcontents.js

--- lockscreen_mac/pages/desktop/js/addcontents.js
+++ lockscreen_mac/pages/desktop/js/addcontents.js
@@ -4,7 +4,7 @@ function addcontents() {
     if (active == 0) {
 	var div = document.getElementById('taskbar-contents');
 	div.innerHTML += '<link href="css/addedcontents.css" rel="stylesheet" type="text/css">\
-		<a href="../../index.htm">Log Out User...</a>\
+		<a href="../../lockscreen.htm">Log Out User...</a>\
 		<br>\
 		';
     active = 1;
