a658d497883b16eaf8fb9ef1112e4282ef425e01
Matthew <Bean6754@users.noreply.github.com>
2018-10-10 16:22:13 +0100

    Update date.js

--- js/date.js
+++ js/date.js
@@ -12,7 +12,7 @@ function startDate() {
 	var days_string = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 	var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 	
-	document.getElementById("day").innerHTML = days_string[day_string.getDay()] + ", " + months[month.getMonth()] + " " + day.getDate();
+	document.getElementById("date").innerHTML = days_string[day_string.getDay()] + ", " + months[month.getMonth()] + " " + day.getDate();
 	d = setTimeout(function() {
 		startDate()
   }, 500);
