846f221312b883eff5693aa609d7c157e549e3a4
Isabella <Bean6754@users.noreply.github.com>
2022-04-11 16:32:06 +0100

    Update a.json.js

--- en/nynorn/orðabuk/js/a.json.js
+++ en/nynorn/orðabuk/js/a.json.js
@@ -1,1 +1,107 @@
+let a = `[
+{
+	"Name": "ag",
+	"Definition": ["movement forward", "drift"],
+	"Examples": [
+		"ag nordesten frå",
+		"wave-motion from north-east",
+		"norden-/sudenag; hannj er ag her; dað er ikke ag å fiskenon",
+		"the fish will not bite"
+	],
+	"Gender": "nn."
+},
 
+{
+	"Name": "aga",
+	"Definition": ["to have feelings of sickness", "be obliged to vomit"],
+	"Examples": [
+		""
+	],
+	"Gender": "vw."
+},
+
+{
+	"Name": "agga",
+	"Definition": "to incite",
+	"Examples": [
+		"sudenvinden aggaði fisken til strandanar",
+		"(add definition here)"
+	],
+	"Gender": "vw."
+},
+
+{
+  "Name": "agbånd",
+  "Definition": "care for and interest in something",
+  "Examples": [
+    "at taka mukkið agbånd i e-u",
+    "(add definition here)"
+  ],
+  "Gender": "nn."
+},
+
+{
+  "Name": "agisom",
+  "Definition": "frightful",
+  "Examples": [
+    ""
+  ],
+  "Gender": "adj."
+},
+
+{
+  "Name": "agla",
+  "Definition": "to dirty",
+  "Examples": [
+    "at agla veð lorgi [skiti]",
+    "(add definition here)"
+  ],
+  "Gender": "vw."
+},
+
+{
+  "Name": "agla",
+  "Definition": ["to get to blaze", "to work energetically"],
+  "Examples": [
+    "at agla upp birteken",
+    "get the fire blaze up",
+    "at agla inn i hvara vinnu",
+    "to work energetically"
+  ],
+  "Gender": "vw."
+},
+
+{
+  "Name": "agment",
+  "Definition": ["anxiety", "unrest"],
+  "Examples": [
+    ""
+  ],
+  "Gender": "nn."
+},
+
+{
+  "Name": "aker",
+  "Definition": ["corn(field) and hay", "stalks of corn", "something destroyed"],
+  "Examples": [
+    "aker og hø",
+    "corn(field) and hay",
+    "at leggja i aker",
+    "to crush and destroy"
+  ],
+  "Gender": "nm."
+},
+
+{
+  "Name": "akkel",
+  "Definition": "shoulder",
+  "Examples": [
+    ""
+  ],
+  "Gender": "nm."
+}
+
+
+
+
+]`
