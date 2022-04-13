d5131459bf4b2082b4b840ec5d1cd5f551028df8
Isabella <Bean6754@users.noreply.github.com>
2022-04-13 01:37:05 +0100

    Create search.js

---
+++ en/nynorn/ordabuk/js/search.js
@@ -0,0 +1,18 @@
+let data = JSON.parse(a)
+
+function search_words() {
+  let input = document.getElementById('searchbar').value
+  input = input.toLowerCase();
+  let x = document.querySelector('#list-holder');
+  x.innerHTML = ""
+
+  for (i = 0; i < data.length; i++) {
+    let obj = data[i];
+
+    if (obj.Name.toLowerCase().includes(input)) {
+      const elem = document.createElement("li")
+      elem.innerHTML = `${obj.Name} (<span style="color:#0e0;">${obj.Gender}</span>) - ${obj.Definition} <span style="font-size:14px;">(<span style="color:#777;">${obj.Examples}</span>)</span>`
+      x.appendChild(elem)
+    }
+  }
+}
