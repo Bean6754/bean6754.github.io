add9e158bc58a7d986b6018f034f9dcbdf594f3e
Isabella <Bean6754@users.noreply.github.com>
2022-04-20 14:58:47 +0100

    Create o.json.js

---
+++ en/nynorn/ordabuk/js/o.json.js
@@ -0,0 +1,12 @@
+/* Credit to: https://nornlanguage.x10.mx/index.php?nynorn_dic and http://nornlanguage.x10.mx/wiki/index.php/Main_Page */
+let o = `[
+{
+  "Name": "Orknøjar",
+	"Definition": ["Orkney", "the Orkney islands"],
+	"Examples": [
+		"Velkomen til Hoy i sud Orknøjon - ",
+		"Welcome to Hoy in south Orkney"
+	],
+	"Gender": "nf."
+}
+]`
