2a2e349ff6e6441093df6d4b6070550d54e61051
Isabella <Bean6754@users.noreply.github.com>
2022-04-20 14:44:20 +0100

    Update a.json.js

--- en/nynorn/ordabuk/js/a.json.js
+++ en/nynorn/ordabuk/js/a.json.js
@@ -1,20 +1,20 @@
 /* Credit to: https://nornlanguage.x10.mx/index.php?nynorn_dic and http://nornlanguage.x10.mx/wiki/index.php/Main_Page */
 let a = `[
 {
-	"Name": "ag",
-	"Definition": ["movement forward", "drift"],
+	"Name": "Ag",
+	"Definition": ["movement forward ", "drift"],
 	"Examples": [
-		"ag nordesten frå",
-		"wave-motion from north-east",
-		"norden-/sudenag; hannj er ag her; dað er ikke ag å fiskenon",
-		"the fish will not bite"
+		"Ag nordesten frå ",
+		"Wave-motion from north-east ",
+		"Norden-/Sudenag; hannj er ag her; dað er ikke ag å fiskenon ",
+		"The fish will not bite"
 	],
 	"Gender": "nn."
 },
 
 {
-	"Name": "aga",
-	"Definition": ["to have feelings of sickness", "be obliged to vomit"],
+	"Name": "Aga",
+	"Definition": ["to have feelings of sickness ", "be obliged to vomit"],
 	"Examples": [
 		""
 	],
@@ -22,27 +22,27 @@ let a = `[
 },
 
 {
-	"Name": "agga",
+	"Name": "Agga",
 	"Definition": "to incite",
 	"Examples": [
-		"sudenvinden aggaði fisken til strandanar",
+		"Sudenvinden aggaði fisken til strandanar ",
 		"(add definition here)"
 	],
 	"Gender": "vw."
 },
 
 {
-  "Name": "agbånd",
+  "Name": "Agbånd",
   "Definition": "care for and interest in something",
   "Examples": [
-    "at taka mukkið agbånd i e-u",
+    "At taka mukkið agbånd i e-u ",
     "(add definition here)"
   ],
   "Gender": "nn."
 },
 
 {
-  "Name": "agisom",
+  "Name": "Agisom",
   "Definition": "frightful",
   "Examples": [
     ""
@@ -51,29 +51,29 @@ let a = `[
 },
 
 {
-  "Name": "agla",
+  "Name": "Agla",
   "Definition": "to dirty",
   "Examples": [
-    "at agla veð lorgi [skiti]",
+    "At agla veð lorgi [skiti] ",
     "(add definition here)"
   ],
   "Gender": "vw."
 },
 
 {
-  "Name": "agla",
-  "Definition": ["to get to blaze", "to work energetically"],
+  "Name": "Agla",
+  "Definition": ["to get to blaze ", "to work energetically"],
   "Examples": [
-    "at agla upp birteken",
-    "get the fire blaze up",
-    "at agla inn i hvara vinnu",
-    "to work energetically"
+    "At agla upp birteken ",
+    "Get the fire blaze up ",
+    "At agla inn i hvara vinnu ",
+    "To work energetically"
   ],
   "Gender": "vw."
 },
 
 {
-  "Name": "agment",
+  "Name": "Agment",
   "Definition": ["anxiety", "unrest"],
   "Examples": [
     ""
@@ -82,19 +82,19 @@ let a = `[
 },
 
 {
-  "Name": "aker",
+  "Name": "Aker",
   "Definition": ["corn(field) and hay", "stalks of corn", "something destroyed"],
   "Examples": [
-    "aker og hø",
-    "corn(field) and hay",
-    "at leggja i aker",
-    "to crush and destroy"
+    "Aker og hø ",
+    "Corn(field) and hay ",
+    "At leggja i aker ",
+    "To crush and destroy"
   ],
   "Gender": "nm."
 },
 
 {
-  "Name": "akkel",
+  "Name": "Akkel",
   "Definition": "shoulder",
   "Examples": [
     ""
@@ -103,17 +103,17 @@ let a = `[
 },
 
 {
-  "Name": "ala",
-  "Definition": ["to feed", "to rear, support", "to entice"],
+  "Name": "Ala",
+  "Definition": ["to feed ", "to rear, support ", "to entice"],
   "Examples": [
-    "du hever alið kuna",
-    "you have a mothering mother"
+    "Du hever alið kuna ",
+    "You have a mothering mother"
   ],
   "Gender": "vs."
 },
 
 {
-  "Name": "ali",
+  "Name": "Ali",
   "Definition": "young domestic animal",
   "Examples": [
     ""
@@ -122,40 +122,40 @@ let a = `[
 },
 
 {
-  "Name": "all",
+  "Name": "All",
   "Definition": "all",
   "Examples": [
-    "alt i senn",
-    "all at once"
+    "Alt i senn ",
+    "All at once"
   ],
   "Gender": "pron"
 },
 
 {
-  "Name": "all",
+  "Name": "All",
   "Definition": "over",
   "Examples": [
-    "dað er alt",
-    "it is over"
+    "Dað er alt ",
+    "It is over"
   ],
   "Gender": "neu."
 },
 
 {
-  "Name": "altra",
+  "Name": "Altra",
   "Definition": "to hesitate",
   "Examples": [
-    "at altra at gera e-t",
-    "hann myn ikke altra at gera dað"
+    "Et altra at gera e-t ",
+    "Hann/Hun myn ikke altra at gera dað"
   ],
   "Gender": "vw."
 },
 
 {
-  "Name": "amla",
-  "Definition": ["to make little progress", "dabble at job"],
+  "Name": "Amla",
+  "Definition": ["to make little progress ", "dabble at job"],
   "Examples": [
-    "at amla at e-u"
+    "At amla at e-u"
   ],
   "Gender": "vw."
 }
