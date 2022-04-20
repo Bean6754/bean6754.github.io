5df783cbbed9f51376ab3c4f2bdbd7f0dc5ccbf9
Isabella <Bean6754@users.noreply.github.com>
2022-04-20 13:59:11 +0100

    Update search.js

--- en/nynorn/ordabuk/js/search.js
+++ en/nynorn/ordabuk/js/search.js
@@ -1,19 +1,18 @@
-let data_a = JSON.parse(a) + JSON.parse(n);
+let data = JSON.parse(a) + JSON.parse(n);
 
 function search_words() {
-  // A
-  let input_a = document.getElementById('searchbar').value;
-  input_a = input_a.toLowerCase();
-  let x_a = document.querySelector('#list-holder-a');
-  x_a.innerHTML = "";
+  let input = document.getElementById('searchbar').value;
+  input = input.toLowerCase();
+  let x = document.querySelector('#list-holder-a');
+  x.innerHTML = "";
 
-  for (i = 0; i < data_a.length; i++) {
-    let obj_a = data_a[i];
+  for (i = 0; i < data.length; i++) {
+    let obj = data[i];
 
-    if (obj_a.Name.toLowerCase().includes(input_a)) {
-      const elem_a = document.createElement("li");
-      elem_a.innerHTML = `${obj_a.Name} (<span style="color:#0e0;">${obj_a.Gender}</span>) - ${obj_a.Definition} <span style="font-size:14px;">(<span style="color:#777;">${obj_a.Examples}</span>)</span>`;
-      x_a.appendChild(elem_a);
+    if (obj.Name.toLowerCase().includes(input)) {
+      const elem = document.createElement("li");
+      elem.innerHTML = `${obj.Name} (<span style="color:#0e0;">${obj.Gender}</span>) - ${obj.Definition} <span style="font-size:14px;">(<span style="color:#777;">${obj.Examples}</span>)</span>`;
+      x.appendChild(elem);
     }
   }
 }
