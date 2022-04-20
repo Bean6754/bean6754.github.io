ef20c7573dcf2d177aec0ffbc43e6c1b66007273
Isabella <Bean6754@users.noreply.github.com>
2022-04-20 15:30:43 +0100

    Create h.json.js

---
+++ en/nynorn/ordabuk/js/h.json.js
@@ -0,0 +1,12 @@
+/* Credit to: https://nornlanguage.x10.mx/index.php?nynorn_dic and http://nornlanguage.x10.mx/wiki/index.php/Main_Page */
+let h = `[
+{
+  "Name": "Hjetland",
+	"Definition": ["Shetland", "the Shetland islands"],
+	"Examples": [
+		"Velkomen til Lervik i sud Hjetlandi - ",
+		"Welcome to Lerwick in south Shetland"
+	],
+	"Gender": "nf."
+}
+]`
