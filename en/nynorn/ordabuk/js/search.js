72412ecb305f2470e59dc02bb790310827bb0168
Isabella <Bean6754@users.noreply.github.com>
2022-04-20 13:42:31 +0100

    Update search.js

--- en/nynorn/ordabuk/js/search.js
+++ en/nynorn/ordabuk/js/search.js
@@ -1,6 +1,6 @@
 let data_a = JSON.parse(a);
 
-function search_words() {
+function search_words_a() {
   let input_a = document.getElementById('searchbar').value;
   input_a = input_a.toLowerCase();
   let x_a = document.querySelector('#list-holder-a');
