5dc52f40e4280fe93ffc54116eeb87078e7b5396
Isabella <Bean6754@users.noreply.github.com>
2022-04-20 12:56:17 +0100

    Create n.json.js

---
+++ en/nynorn/ordabuk/js/n.json.js
@@ -0,0 +1,30 @@
+/* Credit to: https://nornlanguage.x10.mx/index.php?nynorn_dic and http://nornlanguage.x10.mx/wiki/index.php/Main_Page */
+let n = `[
+{
+  "Name": "nord",
+	"Definition": ["north", "northern"],
+	"Examples": [
+		"velkomen til unst i nord hjetlandi"
+	],
+	"Gender": "nn."
+}
+
+{
+  "Name": "norn",
+	"Definition": ["norn language", "an extinct north germanic language that was spoken in orkney, shetland and caithness"],
+	"Examples": [
+		"hann/hun talaði norn"
+	],
+	"Gender": "nf."
+}
+
+{
+  "Name": "nynorn",
+	"Definition": ["new norn language", "a revival project of the extinct norn language"],
+	"Examples": [
+		"eg tala nynorn"
+	],
+	"Gender": "nf."
+}
+
+]`
