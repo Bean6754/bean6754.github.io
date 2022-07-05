9b5609b4a4df811db078f6bf3e39da87ce4cd8a3
Isabella <Bean6754@users.noreply.github.com>
2022-07-05 16:43:50 +0100

    Create f.json.js

---
+++ en/nynorn/ordabuk/js/f.json.js
@@ -0,0 +1,12 @@
+/* Credit to: https://nornlanguage.x10.mx/index.php?nynorn_dic and http://nornlanguage.x10.mx/wiki/index.php/Main_Page */
+let f = `[
+{
+  "Name": "fisk",
+	"Definition": ["Fish"],
+	"Examples": [
+		"Eg eta fisk",
+		"I eat fish"
+	],
+	"Gender": "nm."
+}
+]`
