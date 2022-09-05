17b3d10a1c5d27babc9798f9ec08b509dab3fb9e
Isabella <Bean6754@users.noreply.github.com>
2022-09-05 09:54:36 +0100

    Create aa.json.js

---
+++ en/nynorn/ordabuk/js/aa.json.js
@@ -0,0 +1,12 @@
+/* Credit to: https://nornlanguage.x10.mx/index.php?nynorn_dic and http://nornlanguage.x10.mx/wiki/index.php/Main_Page */
+let aa = `[
+{
+  "Name": "Å",
+	"Definition": ["On", "In"],
+	"Examples": [
+		"Du skriva å norn - ",
+		"You write in norn "
+	],
+	"Gender": "nf."
+}
+]`
