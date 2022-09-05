2c26025d2b10d538a6ca94f051bb4dd3eab14c66
Isabella <Bean6754@users.noreply.github.com>
2022-09-05 09:40:15 +0100

    Create t.json.js

---
+++ en/nynorn/ordabuk/js/t.json.js
@@ -0,0 +1,12 @@
+/* Credit to: https://nornlanguage.x10.mx/index.php?nynorn_dic and http://nornlanguage.x10.mx/wiki/index.php/Main_Page */
+let t = `[
+{
+  "Name": "Takk",
+	"Definition": ["Thank you", "Thanks"],
+	"Examples": [
+		"Takk og du? - ",
+		"Thanks and you?"
+	],
+	"Gender": "nf."
+}
+]`
