e7a1c51a0d8882bcf83dc115bb2afc919ca7c293
Isabella <Bean6754@users.noreply.github.com>
2022-10-21 15:46:39 +0100

    Create s.json.js

---
+++ en/nynorn/ordabuk/js/s.json.js
@@ -0,0 +1,12 @@
+let s = `[
+{
+	"Name": "Sjå",
+	"Definition": ["See", "to see"],
+	"Examples": [
+		"Eg sjå stur fugl - ",
+		"I see big birds. "
+    "Sjå itse - ",
+    "See also."
+	],
+	"Gender": "vv."
+}
