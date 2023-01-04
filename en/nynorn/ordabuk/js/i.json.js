8f15ee5ec9399a97bee962a25c0f0d0a3ca827cd
Bean6754 <bean6754@gmail.com>
2023-01-04 20:23:14 +0000

    Update: 2023_01_04-20_23_14

--- en/nynorn/ordabuk/js/i.json.js
+++ en/nynorn/ordabuk/js/i.json.js
@@ -1,5 +1,14 @@
 let i = `[
 {
+  "Name": "Idrutt",
+    "Definition": ["Sport"],
+    "Examples": [
+      "Idrutt og idrutten - ",
+      "Sport and sports"
+    ],
+    "Gender": "nf."
+},
+{
   "Name": "Itse",
 	"Definition": ["Also", "as well as"],
 	"Examples": [
